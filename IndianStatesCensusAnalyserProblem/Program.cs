80d89c733d7103c91a789318019ee00800df996a
“akashmane3938” <maneakash3938@gmail.com>
2023-01-03 00:50:17 -0800

    [Akash]Reading Data From CSV File

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{
+    internal class Program
+    {
+        public static string stateCensusCSVFilePath = @"E:\Practice\Indian_Census_Program\Indian_States_Census_AnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCensusData.csv";
+
+        
+        static void Main(string[] args)
+        {
+            CSVStateCensus cSVStateCensus = new CSVStateCensus();
+            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath); //Reading StateCensus CSV File
+
+           
+        }
+    }
+}
