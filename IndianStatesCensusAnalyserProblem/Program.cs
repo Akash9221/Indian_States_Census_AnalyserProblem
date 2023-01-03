a36b1861fd3b9df629f2d5273e217201fbbd0da2
“akashmane3938” <maneakash3938@gmail.com>
2023-01-03 01:00:49 -0800

    [Akash]Reading Data From State Code File

--- IndianStatesCensusAnalyserProblem/Program.cs
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -4,13 +4,14 @@
     {
         public static string stateCensusCSVFilePath = @"E:\Practice\Indian_Census_Program\Indian_States_Census_AnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCensusData.csv";
 
-        
+        public static string stateCodeCSVFilePath = @"E:\Practice\Indian_Census_Program\Indian_States_Census_AnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCode.csv";
         static void Main(string[] args)
         {
             CSVStateCensus cSVStateCensus = new CSVStateCensus();
             cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath); //Reading StateCensus CSV File
 
-           
+            CSVStateCode cSVStateCode = new CSVStateCode();
+            cSVStateCode.ReadStateCodeData(stateCodeCSVFilePath); //Reading StateCode CSV File
         }
     }
 }
